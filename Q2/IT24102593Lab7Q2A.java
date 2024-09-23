873def9dc4afefd831b81826ae83aad1b1348e09
IT24102593 <it24102593@my.sliit.lk>
2024-09-24 00:08:16 +0530

    IT24102593 Induwara H.M.D

---
+++ Q2/IT24102593Lab7Q2A.java
@@ -0,0 +1,17 @@
+public class IT24102593Lab7Q2A {
+
+    public static void main(String[] args) {
+
+       for (int i = 0; i < 4; i++ ) {
+
+	for (int j = 0; j < 5; j++) {
+	    System.out.print("$ ");
+
+	}
+	
+	System.out.println();
+
+       } 	
+  }
+
+}
---
+++ Q2/IT24102593Lab7Q2B.java
@@ -0,0 +1,21 @@
+public class IT24102593Lab7Q2B {
+
+    public static void main(String[] args) {
+
+       for (int i = 1; i <= 5; i++ ) {
+
+	System.out.print(i + " - ");
+
+
+
+	for (int j = 0; j < i; j++) {
+	    System.out.print("*");
+
+	}
+	
+	System.out.println();
+
+       } 	
+  }
+
+}
